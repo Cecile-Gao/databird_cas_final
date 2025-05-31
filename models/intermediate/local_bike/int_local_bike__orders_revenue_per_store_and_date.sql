f4ee7933429600788a273f0ef5086f068fa00a14
Clark Kent <just.another.clark.kent@gmail.com>
2025-05-31 16:34:45 +0000

    casted value as in tfor test of accepted values in int yml file

--- models/intermediate/local_bike/int_local_bike__orders_revenue_per_store_and_date.sql
+++ models/intermediate/local_bike/int_local_bike__orders_revenue_per_store_and_date.sql
@@ -3,7 +3,7 @@ SELECT
   o.order_id,
   cust_state, 
   order_date,
-  order_status,
+  CAST(order_status AS string) AS order_status,
   ROUND(SUM(quantity*list_price),2) AS total_order_amount_before_discount, 
   ROUND(SUM(discount*list_price),2) as total_order_discount,
   ROUND(ROUND(SUM(quantity*list_price),2) - ROUND(SUM(discount*list_price),2),2) as total_order_amount_after_discount, 
